66dae04594f4a55955fbe304402ab11b7ce31cb6
Cristian Penagos <suarezalejo26@gmail.com>
2024-01-16 09:51:24 -0500

    v4

---
+++ JS.js
@@ -0,0 +1,68 @@
+
+
+
+ScrollReveal().reveal('.title', {
+    delay: 300,
+    duration: 350,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('section', {
+    delay: 375,
+    duration: 400,
+    reset: true,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.valores', {
+    delay: 375,
+    duration: 450,
+    reset: true,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.politicas-img', {
+    delay: 475,
+    duration: 450,    
+    easing: 'ease-out',
+    origin: 'left',
+    distance: '-500px',
+    reset: true
+});
+
+ScrollReveal().reveal('.politicas', {
+    delay: 375,
+    duration: 450,
+    reset: true,
+    easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.more', {
+    delay: 375,
+    duration: 550,
+    reset: true,
+    easing: 'ease-out'
+});
+
+ScrollReveal().reveal('iframe', {
+    delay: 375,
+    duration: 650,
+    reset: true,
+    easing: 'ease-out'
+});
+
+ScrollReveal().reveal('footer', {
+    delay: 375,
+    duration: 650,
+    reset: true,
+    easing: 'ease-out',
+    origin: 'bottom',
+    distance: '10px'
+});
+
+ScrollReveal().reveal('.btn-portal', {
+    delay: 375,
+    duration: 550,
+    reset: true,
+    easing: 'ease-in'
+});
