9739ba7906ee245e48049d46f2789f59ca18fa89
Cristian Penagos <suarezalejo26@gmail.com>
2024-02-01 14:44:30 -0500

    mobile navbar

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//Navbar Mobile
+const nav = document.querySelector("#nav");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+
+open.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+close.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
 
 ScrollReveal().reveal('.title', {
     delay: 300,
