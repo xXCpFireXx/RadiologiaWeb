9b3a909db5b4105c02d8979f6525c58d5676a0dc
Cristian Penagos <suarezalejo26@gmail.com>
2024-02-23 16:35:42 -0500

    V9

--- main.js
+++ main.js
@@ -88,59 +88,86 @@ ScrollReveal().reveal('.preparaciones h4', {
     easing: 'ease-in'
 });
 
-function formCR() {
+// function formCR() {
     
-    var status = document.getElementById("mot"); 
+//     var status = document.getElementById("mot"); 
 
 
-    if (status.value == "CR") {
-        document.getElementById("form2").style.display = "block";
-    } else {
-        document.getElementById("form2").style.display = "none";
-    }
-}
-//Slider
-const btnLeft = document.querySelector(".btn-left"),
-      btnRight = document.querySelector(".btn-right"),
-      slider = document.querySelector("#slider"),
-      sliderSection = document.querySelectorAll(".slider-section");
+//     if (status.value == "CR") {
+//         document.getElementById("form2").style.display = "block";
+//     } else {
+//         document.getElementById("form2").style.display = "none";
+//     }
+// }
+
+// Add event listener to the form submission
+    document.getElementById("ContactUsForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Perform an AJAX request to submit the form
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", this.action);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {          
+          document.getElementById("ContactUsForm").reset(); //Clear the form fields
+        } else {
+          // Error response
+          showModal("Error: Something went wrong."); // Show a generic error message
+        }
+      }
+    };
+        xhr.send(new FormData(this));
+    });
 
 
-btnLeft.addEventListener("click", e => moveToLeft())
-btnRight.addEventListener("click", e => moveToRight())
+
+
+
+
+    //Slider
+    const btnLeft = document.querySelector(".btn-left"),
+        btnRight = document.querySelector(".btn-right"),
+        slider = document.querySelector("#slider"),
+        sliderSection = document.querySelectorAll(".slider-section");
+
+
+    btnLeft.addEventListener("click", e => moveToLeft())
+    btnRight.addEventListener("click", e => moveToRight())
 
 // setInterval(() => {
 //     moveToRight()
 // }, 3000);
 
 
-let operacion = 0,
-    counter = 0,
-    widthImg = 100 / sliderSection.length;
+    let operacion = 0,
+        counter = 0,
+        widthImg = 100 / sliderSection.length;
 
-function moveToRight() {
-    if (counter >= sliderSection.length-1) {
-        counter = 0;
-        operacion = 0;
+    function moveToRight() {
+        if (counter >= sliderSection.length-1) {
+            counter = 0;
+            operacion = 0;
+            slider.style.transform = `translate(-${operacion}%)`;
+            return;
+        } 
+        counter++;
+        operacion = operacion + widthImg;
         slider.style.transform = `translate(-${operacion}%)`;
-        return;
-    } 
-    counter++;
-    operacion = operacion + widthImg;
-    slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease .6s"
-}  
+        slider.style.transition = "all ease .6s"
+    }  
 
-function moveToLeft() {
-    counter--;
-    if (counter < 0 ) {
-        counter = sliderSection.length-1;
-        operacion = widthImg * (sliderSection.length-1)
+    function moveToLeft() {
+        counter--;
+        if (counter < 0 ) {
+            counter = sliderSection.length-1;
+            operacion = widthImg * (sliderSection.length-1)
+            slider.style.transform = `translate(-${operacion}%)`;
+            return;
+        } 
+        
+        operacion = operacion - widthImg;
         slider.style.transform = `translate(-${operacion}%)`;
-        return;
-    } 
-    operacion = operacion - widthImg;
-    slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease .6s"   
-}   
+        slider.style.transition = "all ease .6s"   
+    }   
 
